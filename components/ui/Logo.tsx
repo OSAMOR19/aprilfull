2c4290d43c90efab459bee194b1f92d66f9ca5cd
temijopelo <temijeje@gmail.com>
2026-01-12 16:43:15 +0100

    update

--- components/ui/Logo.tsx
+++ components/ui/Logo.tsx
@@ -11,7 +11,7 @@ export function Logo() {
         alt="AprilFull"
         width={120}
         height={40}
-        className="h-8 w-auto dark:hidden"
+        className="h-8 w-auto block dark:hidden"
         priority
         loading="eager"
       />
