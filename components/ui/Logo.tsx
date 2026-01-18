19832e01d3e8e8307f2b823f9aa621b99301ecbf
temijopelo <temijeje@gmail.com>
2026-01-18 21:34:59 +0100

    update

--- components/ui/Logo.tsx
+++ components/ui/Logo.tsx
@@ -5,6 +5,16 @@ import Image from "next/image";
 export function Logo() {
   return (
     <Link href="/" className="flex items-center space-x-2">
+      {/* Dark mode logo */}
+      <img
+        src="/images/logo-white.svg"
+        alt="AprilFull"
+        width={120}
+        height={40}
+        className="h-8 w-auto hidden dark:block"
+        loading="eager"
+      />
+
       {/* Light mode logo */}
       <img
         src="/images/logo-dark.svg"
@@ -12,16 +22,6 @@ export function Logo() {
         width={120}
         height={40}
         className="h-8 w-auto block dark:hidden"
-        loading="eager"
-      />
-
-      {/* Dark mode logo */}
-      <img
-        src="/images/logo-white.svg"
-        alt="AprilFull"
-        width={120}
-        height={40}
-        className="h-8 w-auto "
         loading="eager"
       />
     </Link>
